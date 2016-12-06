deccdfcf5fa602e4e57683a60456272a1bde14b3
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-12-07 00:26:18 +0530

    Added websocket

--- watch.go
+++ watch.go
@@ -1,6 +1,10 @@
 package MarkGen
 
-import "time"
+import (
+	"io/ioutil"
+	"os"
+	"time"
+)
 
 const (
 	WatcherInterval = 500
@@ -22,4 +26,49 @@ type Watch struct {
 func newWatch(path string) *Watch {
 	dataChan := DataChan{make(chan *[]byte, DataChanSize), make(chan bool)}
 	return &Watcher{path, nil, nil, &dataChan}
+}
+
+func (w *Watch) Start() {
+	go func() {
+		w.ticker = time.NewTicker(time.Millisecond * WatcherInterval)
+		defer w.ticker.Stop()
+		w.stop = make(chan bool)
+		var currentTimestamp int64
+		for {
+			select {
+			case <-w.stop:
+				return
+			case <-w.ticker.C:
+				reload := false
+				select {
+				case <-w.C.Req:
+					reload = true
+				default:
+				}
+
+				info, err := os.Stat(w.path)
+				if err != nil {
+					continue
+				}
+
+				timestamp := info.ModTime().Unix()
+				if currentTimestamp < timestamp || reload {
+					currentTimestamp = timestamp
+
+					raw, err := ioutil.ReadFile(w.path)
+					if err != nil {
+						continue
+					}
+
+					w.C.Raw <- &raw
+				}
+			}
+		}
+	}()
+
+}
+
+func (w *Watch) Stop() {
+	w.stop <- true
+
 }
