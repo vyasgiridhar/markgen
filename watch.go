20b5f63c835eb37bfb9a011ef1bde4cc8a0a2a89
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-12-09 08:29:38 +0530

    Added file watch

---
+++ watch.go
@@ -0,0 +1,72 @@
+package markgen
+
+import (
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+const (
+	WatcherInterval = 500
+	DataChanSize    = 10
+)
+
+type DataChan struct {
+	Raw chan *[]byte
+	Req chan bool
+}
+
+type Watcher struct {
+	path   string
+	ticker *time.Ticker
+	stop   chan bool
+	C      *DataChan
+}
+
+func NewWatcher(path string) *Watcher {
+	dataChan := DataChan{make(chan *[]byte, DataChanSize), make(chan bool)}
+	return &Watcher{path, nil, nil, &dataChan}
+}
+
+func (w *Watcher) Start() {
+	go func() {
+		w.ticker = time.NewTicker(time.Millisecond * WatcherInterval)
+		defer w.ticker.Stop()
+		w.stop = make(chan bool)
+		var currentTimestamp int64
+		for {
+			select {
+			case <-w.stop:
+				return
+			case <-w.ticker.C:
+				reload := false
+				select {
+				case <-w.C.Req:
+					reload = true
+				default:
+				}
+
+				info, err := os.Stat(w.path)
+				if err != nil {
+					continue
+				}
+
+				timestamp := info.ModTime().Unix()
+				if currentTimestamp < timestamp || reload {
+					currentTimestamp = timestamp
+
+					raw, err := ioutil.ReadFile(w.path)
+					if err != nil {
+						continue
+					}
+
+					w.C.Raw <- &raw
+				}
+			}
+		}
+	}()
+}
+
+func (w *Watcher) Stop() {
+	w.stop <- true
+}
