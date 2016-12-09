cdf399f2efae0eaeec1c0ddbd98937d6c37404f8
vyasgiridhar <vyasgiridhar27@gmail.com>
2016-12-09 08:42:42 +0530

    Added Markdown Generator

--- markgen.go
+++ markgen.go
@@ -1,5 +1,11 @@
 package markgen
 
+import (
+	"fmt"
+
+	"github.com/skratchdot/open-golang/open"
+)
+
 const (
 	Version = "0.1"
 )
@@ -9,7 +15,26 @@ func NewMarkgen(port int) *MarkGen {
 }
 
 type MarkGen struct {
-	port       int
-	httpServer *MarkdownServer
-	stop       chan bool
+	port   int
+	Server *MarkdownServer
+	stop   chan bool
+}
+
+func (m *MarkGen) Run(files ...string) {
+	m.Server = NewMarkdownServer(m.port)
+	m.Server.Listen()
+
+	for _, file := range files {
+		addr := fmt.Sprintf("http://localhost:%d/%s", m.port, file)
+		open.Run(addr)
+	}
+
+	<-m.stop
+
+}
+
+func (m *MarkGen) Stop() {
+	m.Server.Stop()
+	m.stop <- true
+
 }
